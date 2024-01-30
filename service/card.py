6e972b46e60e84cf6b987d661878340ae354bbbe
freewaydev <social@freewayml.com>
2024-01-30 22:13:30 +0100

    add_docstrings

--- service/card.py
+++ service/card.py
@@ -4,6 +4,8 @@ class Card:
         self.value = value
 
     def get_value(self):
+        """Return the value of the card, treating face cards as 10 and aces as
+11."""
         if self.value in ["J", "Q", "K"]:
             return 10
         if self.value == "A":
