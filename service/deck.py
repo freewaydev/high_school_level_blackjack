b9659c253e18833c2e1b926caebdcc7e9d37358e
freewaydev <social@freewayml.com>
2024-02-12 22:25:50 +0100

    add_docstrings

--- service/deck.py
+++ service/deck.py
@@ -2,17 +2,35 @@ import random
 from service.card import Card
 
 class Deck:
+    """
+    Represents a deck of playing cards.
+    Attributes:
+        cards (list): A list of Card instances.
+    """
     def __init__(self):
         self.cards = []
         self.build()
 
     def build(self):
+        """
+        Builds the deck by creating Card instances for each suit and
+        value combination.
+        """
         for suit in ["HEART", "DIAMOND", "CLUB", "SPADE"]:
             for value in ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]:
                 self.cards.append(Card(suit, value))
 
     def shuffle(self):
+        """
+        Shuffles the deck using the random.shuffle function.
+        """
         random.shuffle(self.cards)
 
     def draw(self):
+        """
+        Draws and returns a card from the deck by popping the last item
+        in the cards list.
+        Returns:
+            Card: The drawn card.
+        """
         return self.cards.pop()
