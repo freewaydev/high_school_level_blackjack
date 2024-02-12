9620a701a6665b9d38d181e8564f303f3783c149
freewaydev <social@freewayml.com>
2024-02-12 22:33:00 +0100

    add_docstrings

--- service/user.py
+++ service/user.py
@@ -3,7 +3,11 @@ from typing import Optional
 
 class User:
     """
-    This class represents a user in a game.
+    A class representing a user in a game.
+    Attributes:
+        name (str): The user's name.
+        chips (int): The user's current number of chips.
+        highest_amount (int): The user's highest number of chips ever reached.
     """
 
     def __init__(self, name: str, chips: int = 1000):
@@ -12,17 +16,32 @@ class User:
         self.highest_amount = chips
 
     def bet(self, amount: int):
+        """
+        Place a bet with the given amount.
+        Args:
+            amount (int): The amount to bet.
+        Returns:
+            bool: True if the bet is successful, False otherwise.
+        """
         if amount <= self.chips:
             self.chips -= amount
             return True
         return False
 
     def win(self, amount: int):
+        """
+        Increase the user's chips by the given amount.
+        Args:
+            amount (int): The amount to add to the user's chips.
+        """
         self.chips += amount
         if self.chips > self.highest_amount:
             self.highest_amount = self.chips
 
     def save(self):
+        """
+        Save the user's data to a JSON file.
+        """
         with open("./data/users.json", "r+") as file:
             users = json.load(file)
             users[self.name] = {
@@ -34,6 +53,13 @@ class User:
 
     @classmethod
     def load(cls, name: str) -> Optional['User']:
+        """
+        Load a user's data from a JSON file and return a User instance.
+        Args:
+            name (str): The user's name.
+        Returns:
+            User or None: A User instance if the user is found, None otherwise.
+        """
         with open("./data/users.json", "r") as file:
             users = json.load(file)
             if name in users:
