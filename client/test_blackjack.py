6df4cd9bba894815ab91138d49ea9f68d7d80930
freewaydev <social@freewayml.com>
2024-02-12 22:08:09 +0100

    add_docstrings

--- client/test_blackjack.py
+++ client/test_blackjack.py
@@ -3,12 +3,22 @@ from flask_testing import TestCase
 from app import app, deck, player_hand, dealer_hand
 
 class FlaskAppTest(TestCase):
+    """
+    Test suite for the Flask application that implements a simple card
+    game.
+    """
 
     def create_app(self):
+        """
+        Set up the Flask app for testing.
+        """
         app.config['TESTING'] = True
         return app
 
     def setUp(self):
+        """
+        Initialize the deck and hands for the player and dealer.
+        """
         # Executed before every test
         self.deck = deck
         self.deck.shuffle()
@@ -16,10 +26,16 @@ class FlaskAppTest(TestCase):
         self.dealer_hand = dealer_hand
 
     def tearDown(self):
+        """
+        Clean up after each test.
+        """
         # Executed after every test
         pass
 
     def test_start_game(self):
+        """
+        Test the /start endpoint of the Flask app.
+        """
         response = self.client.get('/start')
         json_response = response.get_json()
         self.assert200(response)
@@ -29,6 +45,9 @@ class FlaskAppTest(TestCase):
         assert len(json_response['dealer']) == 2
 
     def test_hit(self):
+        """
+        Test the /hit endpoint of the Flask app.
+        """
         # Start a game first
         self.client.get('/start')
         response = self.client.get('/hit')
@@ -38,6 +57,9 @@ class FlaskAppTest(TestCase):
         assert len(json_response['player']) >= 3  # Because we've hit once
 
     def test_stand(self):
+        """
+        Test the /stand endpoint of the Flask app.
+        """
         # Start a game first
         self.client.get('/start')
         response = self.client.get('/stand')
