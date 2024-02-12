615950cedda1cd278fdf67ef461f0682d3d47b00
freewaydev <social@freewayml.com>
2024-02-12 22:08:11 +0100

    add_docstrings

--- service/card.py
+++ service/card.py
@@ -1,14 +1,19 @@
 class Card:
     """
-    This class represents a playing card.
+    This class represents a playing card with a suit and value.
     """
     def __init__(self, suit, value):
         self.suit = suit
         self.value = value
 
     def get_value(self):
-        """Return the value of the card, treating face cards as 10 and aces as
-11."""
+        """
+        Return the value of the card as an integer.
+        Face cards (J, Q, K) have a value of 10, and aces have a value of 11.
+        All other cards have their value as an integer.
+        Returns:
+            int: The value of the card.
+        """
         if self.value in ["J", "Q", "K"]:
             return 10
         if self.value == "A":
