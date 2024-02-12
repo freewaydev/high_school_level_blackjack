145dc77f3781e9f8ff66bee741a884cf71c3d694
freewaydev <social@freewayml.com>
2024-02-12 22:33:00 +0100

    add_docstrings

--- client/test_blackjack.py
+++ client/test_blackjack.py
@@ -3,12 +3,24 @@ from flask_testing import TestCase
 from app import app, deck, player_hand, dealer_hand
 
 class FlaskAppTest(TestCase):
+    """
+    A class for testing the functionality of a Flask application.
+    """
 
     def create_app(self):
+        """
+        Creates and configures the Flask app for testing.
+        Returns:
+            Flask app
+        """
         app.config['TESTING'] = True
         return app
 
     def setUp(self):
+        """
+        Executed before every test. Initializes the deck and hands for
+        the player and dealer.
+        """
         # Executed before every test
         self.deck = deck
         self.deck.shuffle()
@@ -16,10 +28,17 @@ class FlaskAppTest(TestCase):
         self.dealer_hand = dealer_hand
 
     def tearDown(self):
+        """
+        Executed after every test. Performs any necessary cleanup.
+        """
         # Executed after every test
         pass
 
     def test_start_game(self):
+        """
+        Tests the /start route. Checks that the response is successful
+        and contains the expected JSON data.
+        """
         response = self.client.get('/start')
         json_response = response.get_json()
         self.assert200(response)
@@ -29,6 +48,10 @@ class FlaskAppTest(TestCase):
         assert len(json_response['dealer']) == 2
 
     def test_hit(self):
+        """
+        Tests the /hit route. Checks that the response is successful
+        and that the player's hand has at least three cards.
+        """
         # Start a game first
         self.client.get('/start')
         response = self.client.get('/hit')
@@ -38,6 +61,10 @@ class FlaskAppTest(TestCase):
         assert len(json_response['player']) >= 3  # Because we've hit once
 
     def test_stand(self):
+        """
+        Tests the /stand route. Checks that the response is successful
+        and contains the expected JSON data.
+        """
         # Start a game first
         self.client.get('/start')
         response = self.client.get('/stand')
